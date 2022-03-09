dfd668f2ce5ed31697b134ab5be8691aa2b5ede2
jtrageser <jtrageser@sodiuswillert.com>
2022-03-09 19:39:21 +0100

    Add PackChk.exe and generatePack.bat

---
+++ generatePack.bat
@@ -0,0 +1,36 @@
+@rem $Id: generatePack.bat 65990 2020-02-27 09:54:45Z jtrageser $
+@rem $URL: https://svn.willert.de/V7/trunk/Component/Product/RXF_Rpy_CPP/RXF/Pack/NotForRelease/generatePack.bat $
+
+@echo off
+
+rem **** Set name of PDSC file to be packed
+dir /b *.pdsc > PDSCName.txt
+set /p PDSCName=<PDSCName.txt
+del /q PDSCName.txt
+
+rem ****  Copy PDSC file to Files directory
+copy /y %PDSCName% .\Source
+
+rem **** Execute a Pack Check
+.\PackChk.exe .\Source\%PDSCName% -n PackName.txt
+
+rem **** Check if PackChk.exe has completed successfully
+if %errorlevel% neq 0 goto ErrPackChk >PackChkLog.txt
+
+rem **** Pipe Pack's Name into Variable
+set /p PackName=<PackName.txt
+
+rem **** Packing
+pushd .\Source
+"C:\Program Files\7-Zip\7z.exe" a ..\Pack\%PackName% -tzip
+popd
+
+goto End
+
+:ErrPackChk
+echo PackChk.exe has encountered an error! >> gen_pack_err.log
+pause
+exit /b
+
+:End
+echo generate of CMSIS Pack completed
